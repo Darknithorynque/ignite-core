58560c3b848c72bceed67723531744d5f52bd448
Darknithorynque <ardamlkc464@gmail.com>
2024-12-23 15:05:26 +0300

    added some untracked files and config

---
+++ src/main/java/com/example/ignite_core/IgniteCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ignite_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ignite_core/IgniteCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ignite_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
