cac0bceb5fa53bd6b06702ec320dfc648e499499
Darknithorynque <ardamlkc464@gmail.com>
2025-03-04 16:02:52 +0300

    created weight log feature

---
+++ src/main/java/com/example/ignite_core/WeightLog/Views.java
@@ -0,0 +1,5 @@
+package com.example.ignite_core.WeightLog;
+
+public class Views {
+    public static class Basic{};
+}
---
+++ src/main/java/com/example/ignite_core/WeightLog/WeightLogController.java
@@ -0,0 +1,58 @@
+package com.example.ignite_core.WeightLog;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/weightLog")
+public class WeightLogController {
+
+    private final WeightLogService weightLogService;
+
+    public WeightLogController(WeightLogService weightLogService) {
+        this.weightLogService = weightLogService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<WeightLogEntity>> getAllWeightLogs() {
+        return weightLogService.getAllWeightLogs();
+    }
+
+    @GetMapping("/between/{userId}")
+    @JsonView(Views.Basic.class)
+    public ResponseEntity<List<WeightLogEntity>> queryWeightLogByUserIdBetween(@PathVariable Long userId, @RequestParam LocalDate from,@RequestParam LocalDate to) {
+        return weightLogService.queryWeightLogsByUserIdBetween(userId,from,to);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WeightLogEntity> getWeightLogById(@PathVariable Long id) {
+        return weightLogService.getWeightLogById(id);
+    }
+
+    @GetMapping("/byUserId/{userId}")
+    @JsonView(Views.Basic.class)
+    public ResponseEntity<List<WeightLogEntity>> getWeightLogsByUserId(@PathVariable Long userId){
+        return weightLogService.getWeightLogByUserId(userId);
+    }
+
+    @PostMapping("/log/{userId}")
+    public ResponseEntity<WeightLogEntity> logAndUpdateWeightLog(@RequestParam double newWeight,@PathVariable long userId) {
+        return weightLogService.logAndUpdateWeight(newWeight,userId);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteWeightLogById(@PathVariable Long id) {
+        return weightLogService.deleteWeightLogById(id);
+    }
+
+    @DeleteMapping("/byUserId/{userId}")
+    public ResponseEntity<String> deleteWeightLogByUserId(@PathVariable Long userId) {
+        return weightLogService.deleteWeightLogsByUserId(userId);
+    }
+}
